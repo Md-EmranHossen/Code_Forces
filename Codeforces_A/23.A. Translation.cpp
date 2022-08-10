abb3528160e774fab834603afa5f9c258c5a03fc
Emran Hossen <92653363+emranhossen01@users.noreply.github.com>
2022-08-11 00:48:59 +0600

    Create 23.A. Translation.cpp

---
+++ Codeforces_A/23.A. Translation.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a,b,c;
+    cin>>a>>b;
+
+
+    reverse(b.begin(),b.end());
+
+
+    if(a==b)
+    {
+
+        cout<<"YES";
+    }
+
+
+    else
+        cout<<"NO";
+
+
+    return 0;
+
+
+
+}
