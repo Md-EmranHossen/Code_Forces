2081e7f3fe8e9978566938cb3dc1c2788f428bed
Emran Hossen <92653363+emranhossen01@users.noreply.github.com>
2023-05-29 11:00:52 +0600

    Create 29.A. Candies and Two Sisters.cpp

---
+++ Codeforces_A/29.A. Candies and Two Sisters.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tst; cin >> tst;
+    while (tst--) {
+        int cndy; cin >> cndy;
+        int ways = (cndy - 1) / 2;
+        cout << ways << '\n';
+
+    }
+    return 0;
+}
