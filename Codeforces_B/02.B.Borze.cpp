f491038d27488b43bffa296dd0c616f87e88cb80
Md. Emran Hossen <92653363+emmranhossen@users.noreply.github.com>
2024-02-04 23:21:27 +0600

    Update 02.B.Borze.cpp

--- Codeforces_B/02.B.Borze.cpp
+++ Codeforces_B/02.B.Borze.cpp
@@ -1,9 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main() {
- 	
- 	
-
+ 	string str; cin >> str;
+ 	int sz = str.size();
+    string ans = "";
+ 	for (int i = 0; i < sz; i++) {
+ 		if (str[i] == '-' && str[i + 1] == '-') {
+     		ans += "2";
+     		i++;
+ 		}
+ 		else if(str[i] == '-' && str[i + 1] == '.') {
+ 			ans += "1";
+ 			i++;
+ 		}
+ 		else {
+ 			ans += "0";
+ 		}
+ 	}
+ 	cout << ans << endl;
 	return 0;
 }
